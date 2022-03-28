3511d48513d04c880234d7d591b02ea5b979c281
Edwin Rozo Gómez <erozog125@gmail.com>
2022-03-28 00:13:43 -0500

    Metodo post

--- src/components/GetCards/GetCards.jsx
+++ src/components/GetCards/GetCards.jsx
@@ -5,25 +5,29 @@ import axios from 'axios';
 
 // Docs: https://tenor.com/developer/dashboard
 // Docs2: https://tenor.com/gifapi/documentation#endpoints
+// api_key: B5S1TNIXJSEY
 
 export const GetCards = () => {
 
   const [gif, setGif] = useState([]);
   const URL = "https://g.tenor.com/v1/search?";
-  const api_key = "api_key=B5S1TNIXJSEY";
+  const key = "B5S1TNIXJSEY";
   const query = "&q="
   const limit = "&limit=16";
 
 
   const getGifs = (event) => {
-    axios.get(`${URL}${api_key}${query}${encodeURI(event.target.value)}${limit}`)    
-    .then(response => {
-      setGif(response.data.data);
-      console.log(response.data.data);      
-    })
-    .catch(error => {
-      console.log(error);
-    })
+    console.log(event.target.value);
+    if (event.keyCode == 13) {
+      console.log('entra');
+      // axios.post(`${URL}${key}${query}${encodeURI(event.target.value)}${limit}`)    
+      fetch(`${URL}${key}${query}${event.target.value}${limit}`)    
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setGif(data.results);
+      })      
+    }
   }
 
   return (
@@ -32,14 +36,14 @@ export const GetCards = () => {
       <hr />
       <InputUI event={getGifs} />
       <main className='main-gif'>
-        {
+        {/* {
           gif.map(card => (
             <GifImg
             key={card.id+card.title}
             image={card.media[0].gif.url}
             name={card.title}
             />))
-        }
+        } */}
       </main>    
     </>
   )
